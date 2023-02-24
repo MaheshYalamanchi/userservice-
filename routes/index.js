7132a29290e375bb5f076d458bac70d35ebb9bc6
varatharajan29062001 <121555475+varatharajan29062001@users.noreply.github.com>
2023-02-24 17:04:11 +0530

    rolecreation ,get, creategroup  ,groupget

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,108 @@
-module.exports = function (params) {
-  var app = params.app;
+var express = require('express');
+var router = express.Router();
 const sharedSevices = require("../routes/sharedService");
 
 
-router.get('/rolecreation', async(req, res) => {
-  
+router.post('/rolecreation', async(req, res) => {
+  "use strict";
+        try {
+          if(req.body.rolename){
+            let result = await sharedSevices.rolecreation(req.body)
+              if (result && result.success) {
+                  res.send({ success: true, message: result.message });
+              } else {
+                  res.send({ success: false, message: result.message });
+              }
+          }else{
+            res.send({ success: false, message: 'Please fill mandatory field.' });
+          }
+           
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
 });
-}
+
+router.get('/get', async(req, res) => {
+  "use strict";
+        try {
+            let result = await sharedSevices.creationget()
+            if (result && result.success) {
+                res.send({ success: true, message: result.message });
+            } else {
+                res.send({ success: false, message: result.message });
+            }
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
+});
+
+router.put('/update', async(req, res) => {
+  "use strict";
+        try {
+            let result = await sharedSevices.roleput(req.body)
+            if (result && result.success) {
+                res.send({ success: true, message: result.message });
+            } else {
+                res.send({ success: false, message: result.message });
+            }
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
+});
+
+router.delete('/delete', async(req, res) => {
+  "use strict";
+        try {
+            let result = await sharedSevices.deleterole(req)
+            if (result && result.success) {
+                res.send({ success: true, message: result.message });
+            } else {
+                res.send({ success: false, message: result.message });
+            }
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
+});
+
+router.post('/creategroup', async(req, res) => {
+  "use strict";
+        try {
+            let result = await sharedSevices.creategroup(req.body)
+            if (result && result.success) {
+                res.send({ success: true, message: result.message });
+            } else {
+                res.send({ success: false, message: result.message });
+            }
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
+});
+
+router.get('/groupget', async(req, res) => {
+  "use strict";
+        try {
+            let result = await sharedSevices.groupget()
+            if (result && result.success) {
+                res.send({ success: true, message: result.message });
+            } else {
+                res.send({ success: false, message: result.message });
+            }
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
+});
+router.put('/groupupdate', async(req, res) => {
+  "use strict";
+        try {
+            let result = await sharedSevices.groupupdate(req.body)
+            if (result && result.success) {
+                res.send({ success: true, message: result.message });
+            } else {
+                res.send({ success: false, message: result.message });
+            }
+        } catch (error) {
+          res.send({ success: false, message: error });
+        }
+});
+
+module.exports = router
--- routes/sharedService.js
+++ routes/sharedService.js
@@ -1,3 +1,173 @@
 const invoke = require("../lib/http/invoke");
+const globalMsg = require('../configuration/messages/message');
+
+let rolecreation = async (params) => {
+  try {
+      const data =  {
+        "rolename": params.rolename,
+        "createdBy": params.createdBy,
+        "createdAt": new Date(),
+        "updatedBy":params.updatedBy,
+        "updatedAt": new Date()
+      }
+      var postdata = {
+        url: process.env.MONGO_URI,
+        client: "role",
+        docType: 0,
+        query: data
+      };
+      let responseData = await invoke.makeHttpCall("post", "write", postdata);
+      if (responseData && responseData.data&&responseData.data.iid) {
+          return { success: true, message: "Record inserted sucessfull"}
+      } else {
+          return { success: false, message: 'Data Not inserted' }
+      }
+  } 
+  catch (error) {
+    if (error && error.code == 'ECONNREFUSED') {
+      return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+      } else {
+         return { success: false, message: error }
+        }
+  }
+};
+
+let creationget = async () => {
+  try {
+      var getdata = {
+        url: process.env.MONGO_URI,
+        client: "role",
+        docType: 1,
+        query: {}
+      };
+      let responseData = await invoke.makeHttpCall("post", "read", getdata);
+      if (responseData && responseData.data) {
+          return { success: true, message: responseData.data.statusMessage }
+      } else {
+          return { success: false, message: 'Data Not Found' }
+      }
+  } 
+  catch (error) {
+    if (error && error.code == 'ECONNREFUSED') {
+      return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+      } else {
+         return { success: false, message: error }
+        }
+  }
+};
+
+let roleput = async (params) => {
+  try {
+      var getdata = {
+        url: process.env.MONGO_URI,
+        client: "role",
+        docType: 0,
+        query: {
+          filter: { "_id": params._id },
+          update: { $set: params.rolename }
+      }
+      };
+      let responseData = await invoke.makeHttpCall("post", "update", getdata);
+      if (responseData && responseData.data) {
+          return { success: true, message: responseData.data.statusMessage }
+      } else {
+          return { success: false, message: 'Data Not Found' }
+      }
+  } 
+  catch (error) {
+    if (error && error.code == 'ECONNREFUSED') {
+      return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+      } else {
+         return { success: false, message: error }
+        }
+  }
+};
+
+let deleterole = async (params) => {
+  try {
+      var getdata = {
+        url: process.env.MONGO_URI,
+        client: "role",
+        docType: 1,
+        query: {
+          _id: params.UserId
+      }
+      };
+      let responseData = await invoke.makeHttpCall("post", "readData", getdata);
+      if (responseData && responseData.data) {
+          return { success: true, message: responseData.data.statusMessage }
+      } else {
+          return { success: false, message: 'Data Not Found' }
+      }
+  } 
+  catch (error) {
+    if (error && error.code == 'ECONNREFUSED') {
+      return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+      } else {
+         return { success: false, message: error }
+        }
+  }
+};
+
+let creategroup = async (params) => {
+  try {
+      const data =  {
+        "groupname": params.groupname,
+        "orgId" : params.orgId
+      }
+      var postdata = {
+        url: process.env.MONGO_URI,
+        client: "group",
+        docType: 0,
+        query: data
+      };
+      let responseData = await invoke.makeHttpCall("post", "write", postdata);
+      if (responseData && responseData.data&&responseData.data.iid) {
+        return { success: true, message: responseData.data.statusMessage }
+      } else {
+          return { success: false, message: 'Data Not inserted' }
+      }
+  } 
+  catch (error) {
+    if (error && error.code == 'ECONNREFUSED') {
+      return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+      } else {
+         return { success: false, message: error }
+        }
+  }
+};
+
+let groupget = async () => {
+  try {
+      var getdata = {
+        url: process.env.MONGO_URI,
+        client: "role",
+        docType: 1,
+        query: {}
+      };
+      let responseData = await invoke.makeHttpCall("post", "read", getdata);
+      if (responseData && responseData.data) {
+          return { success: true, message: responseData.data.statusMessage }
+      } else {
+          return { success: false, message: 'Data Not Found' }
+      }
+  } 
+  catch (error) {
+    if (error && error.code == 'ECONNREFUSED') {
+      return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+      } else {
+         return { success: false, message: error }
+        }
+  }
+};
 
 
+
+module.exports = {
+  rolecreation,
+  creationget,
+  roleput,
+  deleterole,
+  creategroup,
+  groupget
+}
