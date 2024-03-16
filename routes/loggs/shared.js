ba2bde8ef5eae07e3ac28236cf93b085290a31c3
mahesh <mahesh.yalamnchi@lntecc.com>
2024-03-16 17:40:41 +0530

    rewamp for tenant

--- routes/loggs/shared.js
+++ routes/loggs/shared.js
@@ -4,27 +4,27 @@ const { METHODS } = require("http");
 
 let timeincidents = async (params) => {
     try {
-        if(params.peak.peak){
+        if (params.peak.peak) {
             var metrics = params.peak.peak
             var value = params.metrics[metrics];
-            if(value){
+            if (value) {
                 var id = params.room[0].room
                 var postdata = {
-                    url:process.env.MONGO_URI,
-                    database: "proctor",
+                    url: params.tenantResponse.message.connectionString+'/'+params.tenantResponse.message.databaseName,
+					database: params.tenantResponse.message.databaseName,
                     model: "room_log",
                     docType: 0,
                     query: {
-                    filter: { "room": id },
-                    update:{ 
-                        $push: { 
-                        "logmsg": {
-                            "peak": metrics,
-                            "message": value,
-                            "time": params.peak.time
+                        filter: { "room": id },
+                        update: {
+                            $push: {
+                                "logmsg": {
+                                    "peak": metrics,
+                                    "message": value,
+                                    "time": params.peak.time
+                                }
+                            }
                         }
-                        }
-                    }
                     }
                 };
                 let responseData = await invoke.makeHttpCall_userDataService("post", "update", postdata);
@@ -36,35 +36,35 @@ let timeincidents = async (params) => {
             } else {
                 return { success: false, message: 'Metrics are not available' }
             }
-        }else if(params.peak){
-            if(params.peak.submittime){
+        } else if (params.peak) {
+            if (params.peak.submittime) {
                 var time = params.peak.submittime
                 var exam = "Exam submited"
-            }else if(params.peak.starttime){
+            } else if (params.peak.starttime) {
                 var time = params.peak.starttime
                 var exam = "Exam started"
-            }else if (params.peak.pausetime){
+            } else if (params.peak.pausetime) {
                 var time = params.peak.pausetime
                 var exam = "Exam paused"
-            }else if (params.peak.stoptime){
+            } else if (params.peak.stoptime) {
                 var time = params.peak.stoptime
                 var exam = "Exam stoped"
-            }else if (params.peak.messagetime){
+            } else if (params.peak.messagetime) {
                 var time = params.peak.messagetime
                 var exam = params.peak.message
                 var id = params.room[0]._id
                 var postdata = {
-                    url:process.env.MONGO_URI,
-                    database: "proctor",
+                    url: params.tenantResponse.message.connectionString+'/'+params.tenantResponse.message.databaseName,
+					database: params.tenantResponse.message.databaseName,
                     model: "room_log",
                     docType: 0,
                     query: {
                         filter: { "_id": id },
-                        update:{ 
-                            $push: { 
+                        update: {
+                            $push: {
                                 "chat": {
-                                    "sendBy":params.peak.student,
-                                    "role":params.peak.role,
+                                    "sendBy": params.peak.student,
+                                    "role": params.peak.role,
                                     "chatMessage": exam,
                                     "time": time
                                 }
@@ -81,19 +81,19 @@ let timeincidents = async (params) => {
             }
             var id = params.room[0]._id
             var postdata = {
-                url:process.env.MONGO_URI,
-                database: "proctor",
+                url: params.tenantResponse.message.connectionString+'/'+params.tenantResponse.message.databaseName,
+				database: params.tenantResponse.message.databaseName,
                 model: "room_log",
                 docType: 0,
                 query: {
                     filter: { "_id": id },
-                    update:{ 
-                        $push: { 
+                    update: {
+                        $push: {
                             "logmsg": {
                                 "message": exam,
                                 "time": time,
-                                "status" : params.peak.status,
-                                "comment" : params.peak.comment
+                                "status": params.peak.status,
+                                "comment": params.peak.comment
                             }
                         }
                     }
@@ -106,38 +106,38 @@ let timeincidents = async (params) => {
                 return { success: false, message: 'Data Not inserted' }
             }
         }
-    }catch (error) {
-      if (error && error.code == 'ECONNREFUSED') {
-        return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
-      } else {
-        return { success: false, message: error }
-      }
+    } catch (error) {
+        if (error && error.code == 'ECONNREFUSED') {
+            return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+        } else {
+            return { success: false, message: error }
+        }
     }
 };
 let time = async (params) => {
     try {
-        if(params.peak.submittime){
+        if (params.peak.submittime) {
             var time = params.peak.submittime
             var exam = "Exam submited"
-        }else if(params.peak.starttime){
+        } else if (params.peak.starttime) {
             var time = params.peak.starttime
             var exam = "Exam started"
-        }else if (params.peak.stoptime){
+        } else if (params.peak.stoptime) {
             var time = params.peak.stoptime
             var exam = "Exam stoped"
-        }else if (params.peak.messagetime){
+        } else if (params.peak.messagetime) {
             var time = params.peak.messagetime
-            var exam = params.peak.message+"(from="+params.peak.student+")"
-        }else if (params.peak.pausetime){
+            var exam = params.peak.message + "(from=" + params.peak.student + ")"
+        } else if (params.peak.pausetime) {
             var time = params.peak.pausetime
             var exam = "Exam paused"
         }
         const data = {
             "logmsg": {
-                "message" : exam,
-                "time" : time,
-                "status" : params.peak.status,
-                "comment" : params.peak.comment
+                "message": exam,
+                "time": time,
+                "status": params.peak.status,
+                "comment": params.peak.comment
             },
             "room": params.peak.room,
             "student": params.peak.student,
@@ -145,8 +145,8 @@ let time = async (params) => {
             "updatedAt": new Date()
         }
         var postdata = {
-            url:process.env.MONGO_URI,
-            database: "proctor",
+            url: params.tenantResponse.message.connectionString+'/'+params.tenantResponse.message.databaseName,
+			database: params.tenantResponse.message.databaseName,
             model: "room_log",
             docType: 0,
             query: data
@@ -157,72 +157,144 @@ let time = async (params) => {
         } else {
             return { success: false, message: 'Data Not inserted' }
         }
-    }catch (error) {
-      if (error && error.code == 'ECONNREFUSED') {
-        return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
-      } else {
-        return { success: false, message: error }
-      }
+    } catch (error) {
+        if (error && error.code == 'ECONNREFUSED') {
+            return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+        } else {
+            return { success: false, message: error }
+        }
     }
-  }; 
-  let filename = async (params) => {
+};
+let filename = async (params) => {
     try {
-      var getdata = {
-        url:process.env.MONGO_URI,
-        database: "proctor",
-        model: "chats",
-        docType: 1,
-        query: [
-            {
-                $addFields: { b1: "Browser not supported",b2: "Focus changed to a different window",b3: "Full-screen mode is disabled", k1: "Atypical keyboard handwriting",m1: "Microphone muted or its volume is low",
-                    n2: "No connection to a mobile camera", s1: "Screen activities are not shared",s2: "Second display is used", m2: "Conversation or noise in the background", n1: "No network connection",
-                    c1: "Webcam is disabled",c2: "Face invisible or not looking into the camera", c3: "Several faces in front of the camera",c4: "Face does not match the profile", c5: "Found a similar profile",
-                    h1: "Headphone use", m3: "Mobile use"
-                }
-            },
-            {
-            $match: {"room": params.roomId }
-            },
-            {
-                $lookup: {
-                    from: 'attaches',
-                    localField: 'attach',
-                    foreignField: '_id',
-                    as: 'data',
-                }
-            },
-            { $unwind: { "path": "$data","preserveNullAndEmptyArrays": true }},
-            { $match: { "data.attached": true,"data.filename": params.filename } },
-            { $project: {
+        var getdata = {
+            url: process.env.MONGO_URI,
+            database: "proctor",
+            model: "chats",
+            docType: 1,
+            query: [
+                {
+                    $addFields: {
+                        b1: "Browser not supported", b2: "Focus changed to a different window", b3: "Full-screen mode is disabled", k1: "Atypical keyboard handwriting", m1: "Microphone muted or its volume is low",
+                        n2: "No connection to a mobile camera", s1: "Screen activities are not shared", s2: "Second display is used", m2: "Conversation or noise in the background", n1: "No network connection",
+                        c1: "Webcam is disabled", c2: "Face invisible or not looking into the camera", c3: "Several faces in front of the camera", c4: "Face does not match the profile", c5: "Found a similar profile",
+                        h1: "Headphone use", m3: "Mobile use"
+                    }
+                },
+                {
+                    $match: { "room": params.roomId }
+                },
+                {
+                    $lookup: {
+                        from: 'attaches',
+                        localField: 'attach',
+                        foreignField: '_id',
+                        as: 'data',
+                    }
+                },
+                { $unwind: { "path": "$data", "preserveNullAndEmptyArrays": true } },
+                { $match: { "data.attached": true, "data.filename": params.filename } },
+                {
+                    $project: {
                         "_id": 0,
                         "id": "$data._id",
                         "peak": {
-                    $cond: [{ $eq: ["$metadata.peak", "b1"]},"$b1",{ $cond: [ { $eq: ["$metadata.peak","b2" ]},"$b2",{ $cond: [{ $eq: ["$metadata.peak","b3"]},"$b3",{ $cond: [{ $eq: ["$metadata.peak", "k1"]}, "$k1", { $cond: [{ $eq: ["$metadata.peak","m1"]},
-                                    "$m1",{ $cond: [{ $eq: ["$metadata.peak","m2" ] }, "$m2", { $cond: [{ $eq: ["$metadata.peak","m3"]},"$m3", { $cond: [{ $eq: ["$metadata.peak", "n1"]}, "$n1",{ $cond: [ { $eq: ["$metadata.peak","n2"]}, "$n2", { $cond: [ { $eq: ["$metadata.peak", "s1" ]},
-                                    "$s1", { $cond: [{ $eq: [ "$metadata.peak","s2" ]},"$s2",{ $cond: [{ $eq: [ "$metadata.peak", "c1" ]}, "$c1",{ $cond: [{ $eq: [ "$metadata.peak", "c2"]}, "$c2",{ $cond: [{ $eq: ["$metadata.peak","s2"] }, "$s2", { $cond: [{ $eq: ["$metadata.peak", "c3"]},
-                                    "$c3",{ $cond: [{ $eq: [ "$metadata.peak","c4"] },"$c4", { $cond: [{ $eq: [ "$metadata.peak", "c5"]},"$c5",{ $ifNull: ["$metadata.peak",null]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]},"user": "$data.user", "filename": "$data.filename","mimetype": "$data.mimetype",
-                                    "size": "$data.size","createdAt": "$data.createdAt","attached": "$data.attached", "metadata": "$data.metadata"
+                            $cond: [{ $eq: ["$metadata.peak", "b1"] }, "$b1", {
+                                $cond: [{ $eq: ["$metadata.peak", "b2"] }, "$b2", {
+                                    $cond: [{ $eq: ["$metadata.peak", "b3"] }, "$b3", {
+                                        $cond: [{ $eq: ["$metadata.peak", "k1"] }, "$k1", {
+                                            $cond: [{ $eq: ["$metadata.peak", "m1"] },
+                                                "$m1", {
+                                                    $cond: [{ $eq: ["$metadata.peak", "m2"] }, "$m2", {
+                                                        $cond: [{ $eq: ["$metadata.peak", "m3"] }, "$m3", {
+                                                            $cond: [{ $eq: ["$metadata.peak", "n1"] }, "$n1", {
+                                                                $cond: [{ $eq: ["$metadata.peak", "n2"] }, "$n2", {
+                                                                    $cond: [{ $eq: ["$metadata.peak", "s1"] },
+                                                                        "$s1", {
+                                                                            $cond: [{ $eq: ["$metadata.peak", "s2"] }, "$s2", {
+                                                                                $cond: [{ $eq: ["$metadata.peak", "c1"] }, "$c1", {
+                                                                                    $cond: [{ $eq: ["$metadata.peak", "c2"] }, "$c2", {
+                                                                                        $cond: [{ $eq: ["$metadata.peak", "s2"] }, "$s2", {
+                                                                                            $cond: [{ $eq: ["$metadata.peak", "c3"] },
+                                                                                                "$c3", { $cond: [{ $eq: ["$metadata.peak", "c4"] }, "$c4", { $cond: [{ $eq: ["$metadata.peak", "c5"] }, "$c5", { $ifNull: ["$metadata.peak", null] }] }] }]
+                                                                                        }]
+                                                                                    }]
+                                                                                }]
+                                                                            }]
+                                                                    }]
+                                                                }]
+                                                            }]
+                                                        }]
+                                                    }]
+                                            }]
+                                        }]
+                                    }]
+                                }]
+                            }]
+                        }, "user": "$data.user", "filename": "$data.filename", "mimetype": "$data.mimetype",
+                        "size": "$data.size", "createdAt": "$data.createdAt", "attached": "$data.attached", "metadata": "$data.metadata"
+                    }
                 }
-            }
-        ]
-      };
-      let responseData = await invoke.makeHttpCall("post", "aggregate", getdata);
-      if (responseData && responseData.data && responseData.data.statusMessage) {
-        return { success: true, message: responseData.data.statusMessage}
-      } else {
-        return { success: false, message: 'Data Not Found' }
-      }
+            ]
+        };
+        let responseData = await invoke.makeHttpCall("post", "aggregate", getdata);
+        if (responseData && responseData.data && responseData.data.statusMessage) {
+            return { success: true, message: responseData.data.statusMessage }
+        } else {
+            return { success: false, message: 'Data Not Found' }
+        }
     }
     catch (error) {
-      if (error && error.code == 'ECONNREFUSED') {
-        return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
-      } else {
-        return { success: false, message: error }
-      }
+        if (error && error.code == 'ECONNREFUSED') {
+            return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+        } else {
+            return { success: false, message: error }
+        }
     }
-  };
-module.exports={
+};
+
+let tenantResponse = async (params) => {
+    try {
+        if(params.tenantId){
+            var getdata = {
+                url: process.env.MONGO_URI+"/masterdb",
+                database:"masterdb",
+                model: "tenantuser",
+                docType: 1,
+                query:
+                [
+                    {$match:{tenantId: params.tenantId}},
+                    {$lookup:{
+                        from: 'databasemaster',
+                        localField: 'tenantId',
+                        foreignField: 'tenantId',
+                        as: 'data',
+                     }},
+                     { $unwind: { path: "$data", preserveNullAndEmptyArrays: true } },
+                     { $project: {_id:0,tenantId:"$tenantId",connectionString:"$data.connectionString",databaseName:"$data.databaseName"}}
+                ]
+            };
+            let responseData = await invoke.makeHttpCall("post", "aggregate", getdata);
+            if (responseData && responseData.data && responseData.data.statusMessage) {
+                return { success: true, message:responseData.data.statusMessage[0]}
+            } else {
+                return { success: false, message: 'Provide proper tenant params' };
+            }
+        }  else {
+            return { success: false, message: 'Provide proper tenantId' };
+        }
+    } catch (error) {
+        if (error && error.code == 'ECONNREFUSED') {
+            return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+        } else {
+            return { success: false, message: error }
+        }
+    }
+};
+
+module.exports = {
     timeincidents,
     time,
-    filename
+    filename,
+    tenantResponse
 }
